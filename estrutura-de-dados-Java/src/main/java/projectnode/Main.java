59e7194c3b783608d76a5dc022a44f49bf4ff87c
Bribudoj <eduardomsantana@outlook.com.br>
2021-08-23 22:39:40 -0300

    codigo adaptado com generics, e alguns testes com essa forma

--- estrutura-de-dados-Java/src/main/java/projectnode/Main.java
+++ estrutura-de-dados-Java/src/main/java/projectnode/Main.java
@@ -1,20 +1,30 @@
 package projectnode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Object> list = new LinkedList<>();
+        list.add("Queijo");
+        list.add(23241);
 
-        Node node1 = new Node("content Node1");
+        Node<Object> node1 = new Node<>("content Node1");
+        list.add(node1);
 
-        Node node2 = new Node("content Node2");
+        Node<Object> node2 = new Node<>(list);
         node1.setNextNode(node2);
 
-        Node node3 = new Node("content Node3");
+        Node<Object> node3 = new Node<>("content Node3");
         node2.setNextNode(node3);
 
-        Node node4 = new Node("content Node4");
+        Node<Object> node4 = new Node<>(1);
         node3.setNextNode(node4);
 
         System.out.println(node1);
+        System.out.println(node1.getNextNode());
+        System.out.println(node1.getNextNode().getNextNode());
         System.out.println(node1.getNextNode().getNextNode().getNextNode());
     }
 }
--- estrutura-de-dados-Java/src/main/java/projectnode/Node.java
+++ estrutura-de-dados-Java/src/main/java/projectnode/Node.java
@@ -1,28 +1,28 @@
 package projectnode;
 
-public class Node {
+public class Node<T> {
 
-    private String content;
-    private Node nextNode;
+    private T content;
+    private Node<T> nextNode;
 
-    public Node(String content){
+    public Node(T content){
         this.nextNode = null;
         this.content = content;
     }
 
-    public String getContent() {
+    public T getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(T content) {
         this.content = content;
     }
 
-    public Node getNextNode() {
+    public Node<T> getNextNode() {
         return nextNode;
     }
 
-    public void setNextNode(Node nextNode) {
+    public void setNextNode(Node<T> nextNode) {
         this.nextNode = nextNode;
     }
 
